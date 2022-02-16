980bc9e961f044ad4a59c335013902fe9054f408
Agosti Cristian <cristian.wdev@gmail.com>
2022-02-15 21:30:33 -0300

    Commit 2: Rama con animacion de carga

---
+++ js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    const contenedor_carga = document.querySelector ('.contenedor_carga')
+    contenedor_carga.style.opacity = 0 
+    contenedor_carga.style.visibility = 'hidden' 
+
+})
